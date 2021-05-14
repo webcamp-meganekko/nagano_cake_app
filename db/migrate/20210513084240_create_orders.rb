c06013d56a3f7c04295ff36882313ac450af9298
Nebel-hb <nebel.hb@gmail.com>
2021-05-14 13:17:03 +0000

    ログイン機能追加に伴う訂正

--- db/migrate/20210513084240_create_orders.rb
+++ db/migrate/20210513084240_create_orders.rb
@@ -1,7 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
-
+      t.integer :customer_id
+      t.integer :postage
+      t.integer :total_price
+      t.integer :payment_method, default: 0
+      t.string :receve_name
+      t.string :postal_code
+      t.string :address
+      t.integer :order_status, default: 0
       t.timestamps
     end
   end
