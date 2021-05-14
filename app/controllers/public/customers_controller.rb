b94e5714972734103e73dc58cba63dc0e1323557
EC2 Default User <ec2-user@ip-172-31-89-139.ec2.internal>
2021-05-14 09:00:07 +0000

    編集機能実装(仮)

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -5,4 +5,18 @@ class Public::CustomersController < ApplicationController
   
   def edit #@customer = current_user
   end
+  
+  def update
+    @customer = current_customer
+    if @customer.update(customer_params)
+       flash[:success] = "登録情報を変更しました"
+       redirect_to customers_path
+    else
+       render :edit
+    end
+  end
+  
+  def quit_confirm
+  end
+  
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  
+  def full_name(customer)
+    customer.first_name + customer.last_name
+  end
+
+	def kana_full_name(customer)
+		customer.first_name_kana + customer.last_name_kana
+	end
+	
 end
