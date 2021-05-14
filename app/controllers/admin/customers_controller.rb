905152449062e0a7a92296981612c03d9c4e510c
eiji0111 <ee111fender@gmail.com>
2021-05-14 09:59:36 +0000

    [Add]管理者側の会員一覧、編集、詳細画面の追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,2 +1,32 @@
 class Admin::CustomersController < ApplicationController
+  
+  def index
+    @customers = Customer.all
+  end
+  
+  def show
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    @customer = Customer.find(params[:id])
+    @customer = Customer.update(customer_params)
+    redirect_to admin_customers_path
+  end
+  
+  def destroy
+    @customer = Customer.find(params[:id])
+    @customer.destroy
+    redirect_to admin_customers_path
+  end
+  
+  private
+  def customer_params
+     params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
+                                      :email, :is_valid, :postal_code, :address, :tell)
+  end
 end
--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,5 @@
 class Genre < ApplicationRecord
   
+  has_many :products
   validates :genre_name, presence: true
 end
