3d88b67011db7181288c7f4a13c50de9255c01bf
Nebel-hb <ec2-user@ip-172-31-34-165.ec2.internal>
2021-05-14 01:03:46 +0000

    change product_name

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,41 @@
+
 class Admin::ProductsController < ApplicationController
+
+  before_action :authenticate_admin!
+
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to admin_product_path(@product.id)
+
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(product_params)
+    redirect_to admin_product_path(@product.id)
+  end
+
+  private
+
+  def product_params
+    params.require(:products).permit(:image, :product_name, :introduction, :price, :is_active, :genre_id, )
+  end
+
 end
