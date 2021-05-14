c4f8dbeea43ddec7d23f29870a94268685dae794
Nebel-hb <nebel.hb@gmail.com>
2021-05-14 06:42:45 +0000

    [start]cart

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -16,8 +16,8 @@ class Admin::ProductsController < ApplicationController
   def create
     @product = Product.new(product_params)
     @product.save
-    # redirect_to admin_product_path(@product.id)
-    redirect_to admin_products_path
+    redirect_to admin_product_path(@product.id)
+  
   end
 
   def show
