3f534233575f18e7648b76567c1bccf0536175f6
Nebel-hb <ec2-user@ip-172-31-34-165.ec2.internal>
2021-05-14 00:18:20 +0000

    migrate test

--- db/migrate/20210513084947_create_products.rb
+++ db/migrate/20210513084947_create_products.rb
@@ -1,6 +1,13 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
+      t.integer :genre_id
+      t.string :image_id
+      t.text :introduction
+      t.boolean :is_sale
+      t.integer :price
+
+      t.string :products_name
 
       t.timestamps
     end
