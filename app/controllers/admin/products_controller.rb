025ea8df9f21da8a0372caf869926b5d52ba2fc3
Nebel-hb <nebel.hb@gmail.com>
2021-05-14 23:45:14 +0000

    [add]products レイアウト

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -33,7 +33,7 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     @product.update(product_params)
-    # redirect_to admin_product_path(@product.id)
+    redirect_to admin_product_path(@product.id)
   end
 
   private
