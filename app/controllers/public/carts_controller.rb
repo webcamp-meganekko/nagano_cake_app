8c78c8b30fce30244f96956c774ca55ff750fc9d
Nebel-hb <nebel.hb@gmail.com>
2021-05-14 10:06:06 +0000

    [add]cart index

--- app/controllers/public/carts_controller.rb
+++ app/controllers/public/carts_controller.rb
@@ -1,2 +1,39 @@
 class Public::CartsController < ApplicationController
+
+  # before_action :authenticate_customer!
+
+  def index
+    @carts = Cart.all
+  end
+
+  def create
+    @cart = Cart.new(cart_params)
+    @cart.save
+    redirect_to carts_path
+  end
+
+  def update
+    @cart = CartProduct.find(params[:cart][:id])
+    @cart.update(quantity: params[:cart][:quantity])
+    redirect_to carts_path
+  end
+
+  def destroy
+    @cart = Cart.find(params[:id])
+    @cart.destroy
+    redirect_to carts_path
+  end
+
+  def destroy_all
+    # current_customer.carts.destroy_all
+    carts.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def cart_params
+    params.require(:cart).permit(:quantity,:customer_id,:product_id)
+  end
+
 end
