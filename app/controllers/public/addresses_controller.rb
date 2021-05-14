f8b87bc9fb68a76b378806bca5563cdab165a687
eiji0111 <ee111fender@gmail.com>
2021-05-14 03:06:20 +0000

    [Fix]gemfile、css、js一部修正

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,14 +1,37 @@
 class Public::AddressesController < ApplicationController
   
   def index
+    @addresses = Address.all
+  end
+  
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to addresses_path, notice: "配送先を登録しました。"
+    else
+      @addresses = Address.all
+      render 'index'
+    end
   end
   
   def edit
+    @address = Address.find(params[:id])
   end
   
   def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to addresses_path
   end
   
   def destroy
+    address = Address.find(params[:id])
+    address.destroy
+    redirect_to addresses_path
+  end
+  
+  private
+  def address_params
+    params.require(:address).permit(:receve_name, :postal_code, :address)
   end
 end
