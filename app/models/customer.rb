759a928961fc626e00c045442355705c785a1c52
Nebel-hb <nebel.hb@gmail.com>
2021-05-15 01:23:24 +0000

    [start] order

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,15 +3,15 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :addresses, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :carts, dependent: :destroy
   has_many :products, through: :carts
-         
+
   validates :first_name,  presence: true
   validates :last_name, presence: true
-  validates :first_name_kana, :last_name_kana, presence: true, 
+  validates :first_name_kana, :last_name_kana, presence: true,
   format: { with: /\A([ァ-ヶ]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
   # アカウント作成時の苗字名前のカナ入力欄のカタカナのみ入力可能とするバリデーション
   validates :email, presence: true
@@ -20,7 +20,7 @@ class Customer < ApplicationRecord
   validates :address, presence: true
   validates :tell, presence: true, format: { with: /\A\d{10,11}\z/ }
   # アカウント作成時の電話番号はハイフンなしの10桁もしくは11桁のみ登録可能とするバリデーション
-  
+
   # 退会後のログインを禁止(deviseメソッド)
   def active_for_authentication?
     super && (self.is_valid == true)
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,4 @@
 class Order < ApplicationRecord
+  
+  
 end
