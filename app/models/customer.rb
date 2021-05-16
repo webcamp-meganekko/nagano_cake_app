55d8eab510d91d235050bc1409c748c75bd11344
EC2 Default User <ec2-user@ip-172-31-89-139.ec2.internal>
2021-05-16 16:56:24 +0000

    エラーメッセージ、パスの修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -12,13 +12,13 @@ class Customer < ApplicationRecord
   validates :first_name,  presence: true
   validates :last_name, presence: true
   validates :first_name_kana, :last_name_kana, presence: true,
-  format: { with: /\A([ァ-ヶ]|ー)+\z/, message: "カタカナで入力してください" }
+  format: { with: /\A([ァ-ヶ]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
   # アカウント作成時の苗字名前のカナ入力欄のカタカナのみ入力可能とするバリデーション
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i #　←メールアドレスの正規表現
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
-  #メールアドレスを正規表現のフォーム
+  #メールアドレスを正規表現のフォームに指定
   validates :postal_code,  presence: true, format: { with: /\A\d{7}\z/ }
   # アカウント作成時の郵便番号はハイフンなしの7桁のみ登録可能とするバリデーション
   validates :address, presence: true
