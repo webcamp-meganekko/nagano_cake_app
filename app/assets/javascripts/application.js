46fee97d9c27b9c4d91f8d3a90fc5d026cd2c7a4
Nebel-hb <nebel.hb@gmail.com>
2021-05-19 15:54:18 +0000

    [add]btn load

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,24 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require jquery
+//= require jquery_ujs
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+
+$(window).on('turbolinks:load',function(){
+$("#splash").delay(1500).fadeOut('slow',function(){//ローディングエリア（splashエリア）を1.5秒でフェードアウトする記述
+$('body').addClass('appear');//フェードアウト後bodyにappearクラス付与
+var h = $(window).height();//ブラウザの高さを取得
+$(".splashbg").css({
+"border-width":h,//ボーダーの太さにブラウザの高さを代入
+"animation-name":"backBoxAnime"//animation-nameを定義
+});
+});
+$("#splash-logo").delay(1200).fadeOut('slow');//ロゴを1.2秒でフェードアウトする記述
+});
