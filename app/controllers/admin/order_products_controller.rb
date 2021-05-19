f63fd936cb8c602d6641ed76c72dc8cae3e05eb2
EC2 Default User <ec2-user@ip-172-31-89-139.ec2.internal>
2021-05-19 14:02:35 +0000

    注文ステータス実装

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -2,25 +2,14 @@ class Admin::OrderProductsController < ApplicationController
   
   def update
     @order_product = OrderProduct.find(params[:id])
-
     if @order_product.update(order_product_params)
-      if @order_product.making_status == "製作中"
-        @order_product.order.update(order_status: "製作中")
-      elsif @order_product.order.order_products.pluck(:making_status).all?{ |status| status == "製作完了"}
-        @order_product.order.update(order_status: "発送準備中")
-      end
-
+      @order_product.change_order_status
       flash[:notice] = "製作ステータスを変更しました。"
-      redirect_to admin_order_path(@order)
-    elsif @order.order_products.exists?(making_status: ['着手不可','製作待ち','製作中']) == false
-      @order.update(order_status: '発送準備中')
-      flash[:notice] = "製作ステータスを変更しました。"
-      redirect_to admin_order_path(@order)
+      redirect_to admin_order_path(@order_product.order.id)
     else
-      redirect_to admin_order_path(@order)
+      render 'show'
     end
   end
-  
 
   
   private
