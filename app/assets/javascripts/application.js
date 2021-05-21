9829b273640f9cbf5622161481474c22d37f2ce0
EC2 Default User <ec2-user@ip-172-31-18-96.ec2.internal>
2021-05-21 03:27:53 +0000

    about view更新

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -46,3 +46,56 @@ $(function(){
     },time);
   });
 });
+
+
+
+function fadeAnime(){
+
+$('.fadeInUpTrigger').each(function(){
+　　var elemPos = $(this).offset().top-50;
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　　$(this).addClass('fadeIn');
+　　}else{
+　　　$(this).removeClass('fadeIn');
+　　}
+　　});
+
+$('.fadeInDownTrigger').each(function(){
+　　var elemPos = $(this).offset().top-50; 
+　　var scroll = $(window).scrollTop();
+　　var windowHeight = $(window).height();
+　　if (scroll >= elemPos - windowHeight){
+　　$(this).addClass('fadeDown');
+　　}else{
+　　　$(this).removeClass('fadeDown');
+　　}
+　　});
+}
+
+  $(window).scroll(function (){
+    fadeAnime();
+  });
+  
+function fadeAnime(){
+
+  $('.fadeUpTrigger').each(function(){ 
+    var elemPos = $(this).offset().top-50;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll >= elemPos - windowHeight){
+    $(this).addClass('fadeUp');
+    }else{
+    $(this).removeClass('fadeUp');
+    }
+    });
+}
+
+  $(window).scroll(function (){
+    fadeAnime();
+  });
+
+	$(window).on('load', function(){
+		delayScrollAnime();
+	});
