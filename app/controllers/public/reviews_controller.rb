b4f2d117bde422aa219c952302a7284a9e1cc72f
Nebel-hb <nebel.hb@gmail.com>
2021-05-22 09:29:16 +0000

    [fix] レイアウト調整

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,10 +1,11 @@
 class Public::ReviewsController < ApplicationController
-  
+
   def create
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.new(review_params)
-    @review.customer_id = current_customer.id
-    @review.save
+    @review = Review.new
+    review = @product.reviews.new(review_params)
+    review.customer_id = current_customer.id
+    review.save
   end
 
   def index
@@ -18,11 +19,11 @@ class Public::ReviewsController < ApplicationController
     @review.destroy
     render :create
   end
-  
+
   private
 
   def review_params
     params.require(:review).permit(:review)
   end
-  
+
 end
