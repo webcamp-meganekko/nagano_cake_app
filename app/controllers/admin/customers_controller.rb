a6071036564fa002235374a0368f21690b6d4909
eiji0111 <ee111fender@gmail.com>
2021-05-23 10:53:58 +0000

    [Update]before_action set_customer

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,20 +1,18 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_customer, only: [:show, :edit, :update]
   
   def index
     @customers = Customer.page(params[:page]).per(10)
   end
   
   def show
-    @customer = Customer.find(params[:id])
   end
   
   def edit
-    @customer = Customer.find(params[:id])
   end
   
   def update
-    @customer = Customer.find(params[:id])
     if @customer.update(customer_params)
       flash[:notice] = "会員情報を変更しました。"
       redirect_to admin_customers_path
@@ -27,5 +25,9 @@ class Admin::CustomersController < ApplicationController
   def customer_params
      params.require(:customer).permit(:first_name, :last_name, :first_name_kana, :last_name_kana,
                                       :email, :is_valid, :postal_code, :address, :tell)
+  end
+  
+  def set_customer
+    @customer = Customer.find(params[:id])
   end
 end
